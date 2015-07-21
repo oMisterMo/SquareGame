2e3cb9e88201876c6dde33e028ec8e5d7c9b33c5
Mo <Mo@Mo-PC>
2015-07-21 01:54:47 +0100

    sounds when jump

--- src/squaregame/Engine.java
+++ src/squaregame/Engine.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package squaregame;
 
+import java.applet.AudioClip;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -18,6 +18,7 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
+import javax.swing.JApplet;
 import javax.swing.JComponent;
 
 /**
@@ -25,67 +26,80 @@ import javax.swing.JComponent;
  * @author Mo
  */
 public class Engine extends JComponent implements ActionListener, MouseListener {
-    
+
     private int squareSize = 40;
     private int groundLevel = 500;
-    
+
     private int squareX = 40;
     private int squareY = groundLevel - squareSize;
-    
+
     private boolean grounded = true;
     private int jumpSize = 70;
-    
+
     private Image bg;
-    private int bgX = 0;
+    private int bgX = 0;    //background x value
     private int bgY = 201;
+
+    private AudioClip jump;
     
-    
+
     private int num = 0;//onground
     private int num2 = 0;//off ground
-    
-    public Engine(){
+
+    public Engine() {
         try {
             bg = ImageIO.read(getClass().getResource("background2.png"));
         } catch (IOException ex) {
             Logger.getLogger(Engine.class.getName()).log(Level.SEVERE, null, ex);
         }
+
+        jump = JApplet.newAudioClip(getClass().getResource("jump.wav"));
     }
-    
+
+    //FOR DRAWING
     @Override
-    protected void paintComponent(Graphics g){
+    protected void paintComponent(Graphics g) {
         //upgrades g with more functions
-        Graphics2D g2 = (Graphics2D)g;
-        
+        Graphics2D g2 = (Graphics2D) g;
+
         //Sets background color and draws background
         g2.setColor(new Color(205, 255, 140));
         g2.fillRect(0, 0, 600, 600);
         //Draws our background tree
         g2.drawImage(bg, bgX, bgY, null);
-        
+        System.out.println("bgX: " + bgX);
+        if (bgX < -1200) {
+            bgX = 465;
+            //g2.drawImage(bg, bgX2, bgY, null);
+        }
+
         //Draws the ground
-        g2.setColor(new Color(20,20,20));
+        g2.setColor(new Color(20, 20, 20));
         g2.fillRect(0, groundLevel, 600, 500);
-        
+
         //Draws our square
         g2.setColor(Color.BLACK);
         g2.fillRect(squareX, squareY, squareSize, squareSize);
         
+        g2.dispose();
+
     }
 
+    //FOR MOVEMENT
     @Override
     public void actionPerformed(ActionEvent e) {
         //Scroll background
-        bgX -=4;
-        
+        bgX -= 4;
+
         //Checks if square in grounded
-        if(squareY >= groundLevel-squareSize){
-            System.out.println("onGround" + num++);
+        if (squareY >= groundLevel - squareSize) {
+            //System.out.println("onGround" + num++);
             grounded = true;
         }
         //If not on ground, move down
-        if(!grounded){
-            System.out.println("onGround" + num2++);
-            squareY+=8;
+        if (!grounded) {
+            //System.out.println("onGround" + num2++);
+            squareY += 8;
         }
         //squareY--;
         repaint();
@@ -94,14 +108,18 @@ public class Engine extends JComponent implements ActionListener, MouseListener 
     @Override
     public void mouseClicked(MouseEvent e) {
         //grounded = false;
-        squareY -= jumpSize;
-        grounded = false;
+        if (grounded) {
+            squareY -= jumpSize;
+            grounded = false;
+            jump.play();
+        }
+
         repaint();//Repaints duh
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
-        
+
     }
 
     @Override
@@ -115,5 +133,5 @@ public class Engine extends JComponent implements ActionListener, MouseListener 
     @Override
     public void mouseExited(MouseEvent e) {
     }
-    
+
 }
